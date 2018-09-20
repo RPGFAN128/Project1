6bb9f8d609503e98702e0ab7f9ec2601def9ac25
RPGFAN128 <43045923+RPGFAN128@users.noreply.github.com>
2018-09-20 17:55:07 -0600

    Update Project1.java

--- Project1.java
+++ Project1.java
@@ -3,5 +3,10 @@ public class Project1
     public static void main(String[] args) throws Exception
     {
         System.out.println("Hello Git Hub!");
+        
+        for(int i=0; i<10; i++)
+        {
+            
+        }
     }
 }
